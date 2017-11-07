3eede91ec093052cc86c03207b4992a803677b74
Gian Luca Scoccia <loscoccia@gmail.com>
2017-11-07 18:47:38 +0100

    Pre-test page 404 page app not existing page basic css

--- flask_app.py
+++ flask_app.py
@@ -1,14 +1,46 @@
-
-from flask import Flask
+import os.path
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
-# Welcome route
+
+# Default route
 @app.route('/')
-def hello_world():
-    return 'Bella fra!'
+def default():
+    return 'Explanation!'
+
+
+# Pre-test questionaire
+@app.route('/before-test/<appname>')
+def before_explanation(appname):
+    if not activity_file_exists(appname):
+        return render_template('app_not_found.html', appname=appname)
+    return render_template('pre-test.html', appname=appname)
+
+
+# Mappping test
+@app.route('/test/<appname>')
+def test(appname):
+    if not activity_file_exists(appname):
+        return render_template('app_not_found.html', appname=appname)
+    return 'Testooo!'
+
+
+# Post-test questionaire
+@app.route('/after-test/<appname>')
+def after_questionaire(appname):
+    if not activity_file_exists(appname):
+        return render_template('app_not_found.html', appname=appname)
+    return 'Questionaire!'
+
+
+# Utility function for safety url-check
+# Checks whether the app exists in our db, otherwise redirect to error page
+def activity_file_exists(appname: str) -> bool:
+    return os.path.isfile('apps/{}-activities.txt'.format(appname))
+
 
 # Page not found route
-# Pre-test questionaire
-# Mappping test
-# Post-test questionaire
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('page_not_found.html'), 404
